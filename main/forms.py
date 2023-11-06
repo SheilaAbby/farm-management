9b6c5486965f5c2934edf46bbca2ac59387baf08
SheilaAbby <sheilaabigael2@gmail.com>
2023-11-07 00:58:48 +0300

    Updated sign up page

--- main/forms.py
+++ main/forms.py
@@ -7,25 +7,25 @@ from django.contrib.auth.forms import PasswordResetForm
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Enter your email address.', 
                              widget = forms.TextInput(
-                                 attrs = {"class": "form-control"}))
+                                 attrs = {"class": "form-control", "placeholder": "Email"}))
     first_name = forms.CharField(max_length=30, help_text='Required. Enter your first name.',
                                  widget = forms.TextInput(
-                                    attrs = {"class": "form-control"}))
+                                    attrs = {"class": "form-control", "placeholder": "First Name"}))
     last_name = forms.CharField(max_length=30, help_text='Required. Enter your last name.', 
                                 widget = forms.TextInput(
-                                    attrs = {"class": "form-control"}))
+                                    attrs = {"class": "form-control", "placeholder": "Last Name"}))
     username = forms.CharField(max_length=30, help_text='Required. Enter a Username.',
                                 widget = forms.TextInput(
-                                    attrs = {"class": "form-control"}))
-    role = forms.ChoiceField(choices=[('farmer', 'Farmer'), ('field_agent', 'Field Agent'), ('lead_agronomist', 'Lead Agronomist'), ('manager', 'Manager/Staff')],
+                                    attrs = {"class": "form-control", "placeholder": "Username"}))
+    role = forms.ChoiceField(choices=[('', 'Select a Role'),('farmer', 'Farmer'), ('field_agent', 'Field Agent'), ('lead_agronomist', 'Lead Agronomist'), ('manager', 'Manager/Staff')],
                              widget = forms.Select(
                                  attrs = {"class": "form-control"}))
     
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
         # Set the widget attributes for password fields
-        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm Password'})
 
     class Meta:
         model = CustomUser
