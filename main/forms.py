1af96c08d6f57d4ff6c8aab3fe810b19aa2fff47
SheilaAbby <sheilaabigael2@gmail.com>
2023-12-12 21:33:47 +0300

    Adds Search Functionality

--- main/forms.py
+++ main/forms.py
@@ -456,7 +456,7 @@ class FarmPhotoForm(forms.ModelForm):
 class SearchForm(forms.Form):
     query = forms.CharField(
         label='Search...',
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your search query.', 'style': 'width: 300px;'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Search your registered farms...', 'style': 'width: 300px;'})
     )
 
 class MessageForm(forms.Form):
