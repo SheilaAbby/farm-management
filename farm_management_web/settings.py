1efb8db8780316483720e6cca49857f8d41c7f7e
SheilaAbby <sheilaabigael2@gmail.com>
2024-03-06 06:28:06 +0300

    Add windwood domain in settings

--- farm_management_web/settings.py
+++ farm_management_web/settings.py
@@ -135,7 +135,7 @@ if DEVELOPMENT_MODE is True:
             }
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv('DATABASE_URL', None) is None:
-        raise Exception('DATABASE_URL environment variable not defined')
+        raise Exception('DATABASE_URL environment variable not defined!')
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
