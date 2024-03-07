11b5b33f87be2e57b753aeb400de04369269f100
SheilaAbby <sheilaabigael2@gmail.com>
2024-03-08 01:16:22 +0300

    Settings Updated

--- farm_management_web/settings.py
+++ farm_management_web/settings.py
@@ -77,7 +77,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('157.245.103.7', 6379)],
+            "hosts": [('windwoodfarmersnetwork.com', 6379)],
         },
     },
 }
