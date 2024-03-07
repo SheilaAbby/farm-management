9775c7b5cc6c92ae9b35ffdece2d4b4258dc4a6f
SheilaAbby <sheilaabigael2@gmail.com>
2024-03-07 15:39:14 +0300

    Custom JS Updated

--- static/js/custom.js
+++ static/js/custom.js
@@ -56,8 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
           }
       },
       error: function(error) {
-          console.error('Error checking new messages!!!!:', error);
-          console.error('Error details:', error.message, error.stack);
+          console.error('Error checking new messages!:', error);
       }
   });
   
