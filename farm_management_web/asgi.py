e28f48fb6951476e9b45b61f1f081d1c6d70befd
SheilaAbby <sheilaabigael2@gmail.com>
2024-03-08 03:04:41 +0300

    ASGI Updated

--- farm_management_web/asgi.py
+++ farm_management_web/asgi.py
@@ -8,13 +8,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
-django_asgi_app = get_asgi_application()
+import django
+from channels.routing import get_default_application
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from .routing import application as websocket_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_management_web.settings')
+django.setup()
+django_asgi_app = get_default_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
