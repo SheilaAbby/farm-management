b8b7884e69ab68d521ccf70a82894a0e00951c79
SheilaAbby <sheilaabigael2@gmail.com>
2024-03-08 12:19:13 +0300

    Settings Updated

--- farm_management_web/settings.py
+++ farm_management_web/settings.py
@@ -68,7 +68,8 @@ INSTALLED_APPS = [
     'main.apps.MainConfig',
     'crispy_forms',
     'crispy_bootstrap5',
-    'fontawesomefree'
+    'fontawesomefree',
+    'farm_management_web'
 ]
 
 ASGI_APPLICATION = "farm_management_web.routing.application"
@@ -197,11 +198,11 @@ AUTH_USER_MODEL = 'main.CustomUser'
 # URL to redirect to after changing the password
 PASSWORD_RESET_COMPLETE = '/login'
 
-# #  set up to send emails 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True    
-EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = '2525'
+# # #  set up to send emails 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_USE_TLS = True    
+# EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = '2525'
 
