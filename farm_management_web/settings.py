2a3c0bbc3da6cd71d5a56e1a72e535293fa421ec
SheilaAbby <sheilaabigael2@gmail.com>
2024-03-08 12:25:12 +0300

    Settings Updated

--- farm_management_web/settings.py
+++ farm_management_web/settings.py
@@ -78,7 +78,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('157.245.103.7', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     }
 }
