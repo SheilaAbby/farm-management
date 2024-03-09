824247259d2c9e8d243f469f1393fe6bf273a1c8
SheilaAbby <sheilaabigael2@gmail.com>
2024-03-09 22:43:06 +0300

    Routing&ASGI  Updated

--- farm_management_web/asgi.py
+++ farm_management_web/asgi.py
@@ -1,38 +1,10 @@
-"""
-ASGI config for farm_management_web project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
-# from django.core.asgi import get_asgi_application
-# django_asgi_app = get_asgi_application()
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'farm_management_web.settings'
-
 import django
-django.setup()
-
-from django.core.asgi import get_asgi_application
-django_asgi_app = get_asgi_application()
-
-
-from channels.http import AsgiHandler
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from .routing import websocket_application
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_management_web.settings')
 
-application = ProtocolTypeRouter({
-    # "http": AsgiHandler(),
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        websocket_application
-    ),
-})
+django.setup()
+application = get_default_application()
 
 
--- farm_management_web/routing.py
+++ farm_management_web/routing.py
@@ -1,14 +1,25 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from channels.auth import AuthMiddlewareStack
+from channels.auth import AuthMiddlewareStack, OriginValidator, TokenAuthMiddleware
 
 from .consumers import ChatConsumer
 
-websocket_application = ProtocolTypeRouter(
-    {
-        "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path("ws/chat/", ChatConsumer.as_asgi()),
-        ])
+# websocket_application = ProtocolTypeRouter(
+#     {
+#         "websocket": AuthMiddlewareStack(
+#         URLRouter([
+#             path("ws/chat/", ChatConsumer.as_asgi()),
+#         ])
+#     ),
+#     })
+
+application = ProtocolTypeRouter({
+    'websocket': OriginValidator(
+        TokenAuthMiddleware(
+            URLRouter([
+                 path("ws/chat/", ChatConsumer.as_asgi()),
+            ])
+        ),
+        ['*'],
     ),
-    })
+})
