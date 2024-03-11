db68c567b3aedfd1ca505ccb1d9b470be7ca7ab5
SheilaAbby <sheilaabigael2@gmail.com>
2024-03-11 20:43:06 +0300

    Routing,ASGI & Settings  Updated

--- farm_management_web/routing.py
+++ farm_management_web/routing.py
@@ -1,34 +1,14 @@
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.urls import path
-# from channels.auth import AuthMiddlewareStack, OriginValidator, TokenAuthMiddleware
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
 
 from .consumers import ChatConsumer
 
-# # websocket_application = ProtocolTypeRouter(
-# #     {
-# #         "websocket": AuthMiddlewareStack(
-# #         URLRouter([
-# #             path("ws/chat/", ChatConsumer.as_asgi()),
-# #         ])
-# #     ),
-# #     })
-
-# application = ProtocolTypeRouter({
-#     'websocket': OriginValidator(
-#         TokenAuthMiddleware(
-#             URLRouter([
-#                  path("ws/chat/", ChatConsumer.as_asgi()),
-#             ])
-#         ),
-#         ['*'],
-#     ),
-# })
-
-# chat/routing.py
-from django.urls import path
-
-from . import consumers
-
-application = [
-    path("ws/chat/", ChatConsumer.as_asgi()),
-]
+application = ProtocolTypeRouter(
+    {
+        "websocket": URLRouter(
+            [
+                path("ws/chat/", ChatConsumer.as_asgi()),
+            ]
+        ),
+    }
+)
