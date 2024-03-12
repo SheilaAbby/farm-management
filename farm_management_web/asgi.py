58cb54756379f942a25d4e18e5da2dc6d1889c2a
SheilaAbby <sheilaabigael2@gmail.com>
2024-03-12 11:56:27 +0300

    ASGI Updated

--- farm_management_web/asgi.py
+++ farm_management_web/asgi.py
@@ -1,25 +1,39 @@
+# """
+# ASGI config for farm_management_web project.
+
+# It exposes the ASGI callable as a module-level variable named ``application``.
+
+# For more information on this file, see
+# https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+# """
+
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from .routing import application as websocket_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_management_web.settings')
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             websocket_application  
+#         )
+#     ),
+# })
+
 """
-ASGI config for farm_management_web project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
 """
 
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from .routing import application as websocket_application
+import django
+from decouple import config
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_management_web.settings')
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_application  
-        )
-    ),
-})
+django.setup()
+application = get_default_application()
