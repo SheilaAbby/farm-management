39a1e095fb11131fd6064f9a8cc65c06862cebc2
SheilaAbby <sheilaabigael2@gmail.com>
2024-03-13 23:03:57 +0300

    Update Password Reset Email Configs

--- farm_management_web/settings.py
+++ farm_management_web/settings.py
@@ -212,7 +212,7 @@ AUTH_USER_MODEL = 'main.CustomUser'
 # URL to redirect to after changing the password
 PASSWORD_RESET_COMPLETE = '/login'
 
-# # #  set up to send emails 
+# set up to send emails - Dev
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_USE_TLS = True    
 # EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
@@ -220,3 +220,11 @@ PASSWORD_RESET_COMPLETE = '/login'
 # EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # EMAIL_PORT = '2525'
 
+
+# Set up to send emails - Production
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True    
+EMAIL_HOST = 'live.smtp.mailtrap.io'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = '2525'
